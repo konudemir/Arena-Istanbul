3b9fe4a41a7d4841da6e323da9ff467f20a362ef
Oguzhan-AYDIN1 <oguzhanaydin698@gmail.com>
2025-03-31 21:35:31 +0300

    New Screen

---
+++ Screens/ArenaScreen.java
@@ -0,0 +1,10 @@
+package Screens;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import C.CharacterPanel;
+import m.MainFrame;
+public class ArenaScreen extends JPanel {
+    
+}
